166a33e0db31b12535aec4552429386a2165e0d3
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-31 07:32:11 -0600

    Add project files.

---
+++ MiniProjectStudentCheck/Program.cs
@@ -0,0 +1,6 @@
+﻿// Requirements
+// Ask the user for their name and age
+// if their name is Bob or Sue address them as Profrerssor
+// if the person is under 21, recommend they wait X years until their 21 to start this class
+// 
+
