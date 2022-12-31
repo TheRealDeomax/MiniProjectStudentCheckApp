75c74dfd632a93e4e2e8ee55c59a3e9fa4243d8e
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-31 13:13:56 -0600

    format updates

--- MiniProjectStudentCheck/Program.cs
+++ MiniProjectStudentCheck/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Requirements
 // Ask the user for their name and age
 
-Console.Write(value:"What is your name:");
-string? name=Console.ReadLine();
+Console.Write(value: "What is your name:");
+string? name = Console.ReadLine();
 
 Console.Write(value: "How old are you:");
 
@@ -14,25 +14,25 @@ if (name.Length == 0) inputValid = false;
 
 switch (inputValid)
 {
-    case true: 
-        
-            if ( ((name.ToLower() == "sue") || (name.ToLower() == "bob") ) && age >= 21 )
-                    {
+    case true:
+
+        if (((name.ToLower() == "sue") || (name.ToLower() == "bob")) && age >= 21)
+        {
             // if their name is Bob or Sue address them as Profrerssor, otherwise address them by nameConsole.WriteLine(value: "Your age is invalid. I'm sorry.  You cannot attend.");
-            Console.WriteLine(value:"Welcome to the class Profressor!");
-            }
-            else if (age < 21)
-            {
+            Console.WriteLine(value: "Welcome to the class Profressor!");
+        }
+        else if (age < 21)
+        {
             // if the person is under 21, recommend they wait X years until their 21 to start this class
-            Console.WriteLine(value:$"I'm sorry. You must wait {21 - age} years to attend.");
-            }
-            else
-            {
-            Console.WriteLine(value:$"Welcome to the class {name}!");
-            }
+            Console.WriteLine(value: $"I'm sorry. You must wait {21 - age} years to attend.");
+        }
+        else
+        {
+            Console.WriteLine(value: $"Welcome to the class {name}!");
+        }
 
         break;
-     case false:
+    case false:
         Console.WriteLine(value: "I'm sorry.  Either you did't enter your name or your age is invalid. You cannot attend this class.");
         break;
 
